ef42ae76177115c7df573ab6d6f64055b9e24260
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2022-01-10 10:07:25 +0100

    Boletín 18

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -1,8 +1,17 @@
 package com.dam1;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        ConversorTemperaturas obx = new ConversorTemperaturas();
+        try {
+            obx.centigradosAFharenheit();
+            obx.centígradosAReamur();
+        } catch ( TemperaturaErradaExcepcion ex) {
+            JOptionPane.showMessageDialog(null, "ERROR " + ex.toString());
+        }
 
     }
 }
